715eb7d08d261717ef801625d476884f5bd62465
PureParadise56b <toufique26ajay@gmail.com>
2024-01-17 21:45:39 +0530

    file upload setup

---
+++ src/utils/cloudinary.util.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadCloudinary = async (filePath) => {
+  try {
+    if (!filePath) return null;
+
+    // upload file on cloudinary
+    const response = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+
+    // file has been uploaded successfully
+    console.log("File Uploaded On Cloudinary", response.url);
+    return response;
+  } catch (error) {
+    // remove locally temp file as the upload operation got failed
+    fs.unlinkSync(filePath);
+    return null;
+  }
+};
+
+export default uploadCloudinary;
